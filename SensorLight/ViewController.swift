dae7243e2bc8b4f4cb69deef9b3b3eafe289db1a
YukiMaitani <fightzoi1997@gmail.com>
2021-12-11 15:14:30 +0900

    Initial Commit

---
+++ SensorLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SensorLight
+//
+//  Created by 米谷裕輝 on 2021/12/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SensorLightTests/SensorLightTests.swift
@@ -0,0 +1,33 @@
+//
+//  SensorLightTests.swift
+//  SensorLightTests
+//
+//  Created by 米谷裕輝 on 2021/12/11.
+//
+
+import XCTest
+@testable import SensorLight
+
+class SensorLightTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
